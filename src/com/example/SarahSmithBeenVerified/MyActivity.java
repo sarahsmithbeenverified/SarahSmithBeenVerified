c45da73d0fe577ff6e960970a7bb0ff491583ad3
smith325 <sesmith325@gmail.com>
2015-04-21 16:09:02 -0400

    First Commit

---
+++ src/com/example/SarahSmithBeenVerified/MyActivity.java
@@ -0,0 +1,17 @@
+package com.example.SarahSmithBeenVerified;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.Button;
+
+public class MyActivity extends Activity {
+    private Button tab1, tab2, tab3;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
