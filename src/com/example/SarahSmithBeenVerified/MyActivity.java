d4f844b485b89f4ce3c8792cf730e10232dac5c1
smith325 <sesmith325@gmail.com>
2015-04-21 21:56:03 -0400

    Added icons

--- src/com/example/SarahSmithBeenVerified/MyActivity.java
+++ src/com/example/SarahSmithBeenVerified/MyActivity.java
@@ -1,8 +1,10 @@
 package com.example.SarahSmithBeenVerified;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.widget.Button;
 
 public class MyActivity extends Activity {
@@ -13,5 +15,13 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        tab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MyActivity.this,TabTwo.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ src/com/example/SarahSmithBeenVerified/TabTwo.java
@@ -0,0 +1,17 @@
+package com.example.SarahSmithBeenVerified;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+/**
+ * Created by sesmith325 on 4/21/15.
+ */
+public class TabTwo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.id.tab2);
+
+    }
+}
